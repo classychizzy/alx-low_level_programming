f359bbf7bd1847a92f5e56e8bc3a4cb5f4500cd0
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-11 07:17:50 -0800

    use of puts function to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints programming is like building a multilingual puzzle
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
