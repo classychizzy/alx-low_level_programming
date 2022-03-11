1d889f47ae896cdbd26973e547403ff72b1c5b46
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-11 15:39:04 -0800

    assign random number and print the last digit assigned to a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,11 @@ int main(void)
 	last = n % 10;
 	if (last > 5)
 		printf("last digit of %d is %d is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last);
 	else if (last < 6 && last != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
 	return (0);
 }
