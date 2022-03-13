f0a1e559fb39ef54e6b00f2d11e34727a6ee6cc1
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-13 10:14:22 -0700

    program that prints all digits of base 16 in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ * Description: a program that prints all numbers of base 16
+ * in lower case followed by a new line
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	char d = '0';
+
+	while (d <= 9)
+	{
+		putchar(d);
+		d++;
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
