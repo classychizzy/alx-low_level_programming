d63c3a88dfaf23b2b3e6823f15e703a8540c0615
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-14 14:53:13 -0700

    a program that prints all combinations of two digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,25 @@
  *
  */
 int main(void)
+
 {
-	int i;
+	int c, i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(i);
-	}
-
-	if (i != '9')
-	{
-		putchar(',');
-		putchar(' ');
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 
 	putchar('\n');
