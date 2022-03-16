0a65e6e55da58bd90491daa8efeed46a9cef2530
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-16 07:40:33 -0700

    a program that uses a custom function to check for lower case letters

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - entry point
- * Return Always 0
+ * main -Entry point
+ * Description: a function that checks for lowercase letters
+ * Return: always 0
  */
 int main(void)
 {
@@ -10,19 +11,14 @@ int main(void)
 
 	r = _islower('H');
 
-	 _putchar(r + '0');
-	 
-	 r = _islower('o');
-	 
-	 _putchar(r + '0');
-	 
-	 r = _islower(108);
-	 
-	 _putchar(r + '0');
-	 
-	 _putchar('\n');
-	 
-	 return (0);
+	_putchar(r + '0');
 
+	r = _islower(108);
+
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	return (0);
 }
 
