5c71fb4d94fcf1cb1540d86aef5d03b288b26f19
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-17 13:06:20 -0700

    debugging an infinite loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,8 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int i;
-
 	i = 98;
 
 	positive_or_negative(i);
