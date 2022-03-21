263546db8feb17ac2b26c50e789f3bf75488f93f
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-21 13:44:55 -0700

    a program that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string- a function that reverses a string
+ * Description: a function that prints a reverse string
+ * @s: character representing string
+ *
+ */
+void rev_string(char *s)
+{
+	int i; /*variable used for looping*/
+	int j;/*variable used to trim the string */
+	int len;/* variable that stores string length */
+	int temp;/*variable used to temporarily store the value of string*/
+
+	while (s[i] != '\0')
+	{
+		i++ /*pass each member of string to stdout*/
+	}
+
+	length = i; /* i is now the position of each string member*/
+	i = 0;
+	j = length - 1;
+
+	while (i < j)
+	{
+		temp = s[i];/* this helps to interchange the value for j and i*/
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+
