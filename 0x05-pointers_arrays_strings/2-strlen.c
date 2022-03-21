4c64fcc24f3b1a6a3e2c679c7232e348ab7e0bc0
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-21 11:00:13 -0700

    function that calculates the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,17 @@
 /**
  *  _strlen- function to calculate the length of a string
  *  @s: identifier for string
- *  Return: string
+ *  Return: 0 success
  */
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while (*(s + i) != '/0')
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
 
-	return (i);
+	return (0);
 }
