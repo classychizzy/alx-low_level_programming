b9daf865027d72d27492b2094a488177f1ef0dba
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-21 15:52:47 -0700

    a function that prints character of a string starting with first letter followed by a new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,20 +7,16 @@
  */
 void puts2(char *str)
 {
-	int i  = 0;
+	int i, j;
 
-	while (i >= 0)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
+		;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 
+	_putchar(10);
 }
