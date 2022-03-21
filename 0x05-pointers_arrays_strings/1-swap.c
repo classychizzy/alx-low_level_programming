dc12229cd5d726a70505f7286534db6a068d551d
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-21 10:34:03 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int- function with two pointer parameters
+ * Description: a function for swapping the values
+ * of two pointers
+ *
+ * @a: first pointer integer
+ * @b: second pointer integer
+ * @c: swap variable
+ * Return: value
+ *
+ */
+ void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
