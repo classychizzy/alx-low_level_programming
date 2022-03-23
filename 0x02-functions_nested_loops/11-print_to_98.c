59fe026bf29c66bf378f77776392928652345581
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-23 05:17:06 -0700

    debug 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,34 +8,30 @@
 
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
+	int i, j; /* numbers used to get the range of natural numbers*/
+	/* set 1st condition to target values that are > 98*/
+		if (n >= 98) /* target negative values*/
 		{
-			if (i != 98)
+			for (i = n; i >= 98; i--)
 			{
-				printf("%d, ", i);
+				if (i != 98)
+					printf("%d\t", i);
+				else if (i == 98)
+					printf("%d\t", i);
 			}
-			else if (i == 98)
-			{
-				printf("%d\n", i);
-			}
+			
 		}
-	}
-	else if (n >= 98)
+		else if (n <= 98)
 		{
-			for (j = n; j >= 98; j--)
+			for (j = n; j <= 98; j++)
 			{
 				if (j != 98)
-				{
-					printf("%d, ", j);
-				}
+					printf("%d\t", j);
 				else if (j == 98)
-				{
-					printf("%d\n", j);
-				}
+					printf("%d\t", j);
 			}
+			
 		}
+
+		printf("\n");
 }
