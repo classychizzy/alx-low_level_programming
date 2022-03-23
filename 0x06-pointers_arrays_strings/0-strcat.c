fa429d86511719923a99fa7141cc8138ae69a49a
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-23 16:12:36 -0700

    concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+ /**
+  * *_strcat - for concatenating strings
+  *
+  * @dest: first input parameter
+  * @src: second input parameter
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int w; /* getting length of dest*/
+	int x;
+
+	w = 0;
+	while (dest[w] != '\0')
+	{
+		w++;
+
+	}
+
+	x = 0;
+	while (src[x] != '\0')
+	{
+		dest[w] = src[x];
+		w++;
+		x++;
+	}
+	dest[w] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# this file contains information about my first task on working with arrays and strings in c
+
