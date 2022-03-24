737f385538fd21908f9b5ffb57e13c27c7d30a99
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-24 16:53:31 -0700

    an array that converts strings to  uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - a function that converts strings to uppercase
+ * @r :  character that represents lower case letters
+ * Return: (r)
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; r[i] != '\0'; i++)
+	{
+		r[i] -= 32;
+	}
+
+	return c
+}
