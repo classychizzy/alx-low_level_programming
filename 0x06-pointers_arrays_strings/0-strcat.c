530a9b41b2fed3291a432089fdddea4827eec194
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-24 00:08:34 -0700

    a function that concatenates + fix of betty doc issue

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
- /**
-  * _strcat - for concatenating strings
-  *
-  * @dest: first input parameter
-  * @src: second input parameter
-  * Return: dest
-  */
+/**
+ * _strcat -concatenate two strings
+ * @dest: first input parameter
+ * @src: second input parameter
+ * Return: dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	int w; /* getting length of dest*/
