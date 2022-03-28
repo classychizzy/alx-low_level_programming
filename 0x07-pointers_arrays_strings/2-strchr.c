f7a4cb83e6145c7d1774c0a94383eb4283a5c345
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-28 14:32:28 -0700

    a function that searches for the occurence of a character and returns a pointer to the character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; i < (*(s + i)!= '\0'); i++)
+	for (i = 0; i < (*(s + i) != '\0'); i++)
 	{
 		if (*(s + i) == c)
-		      return (s + i);
+			return (s + i);
 	}
 
 	return ('\0');
