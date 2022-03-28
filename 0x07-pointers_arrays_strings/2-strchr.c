41c5d1451a21685719b52cc4f6b91207bcb6bc7d
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-28 14:29:02 -0700

    a function that searches for the occurence of a character and returns a pointer to the character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - a function that returns a pointer to the first occurence of a char
+ * @s: memory area
+ * @c: constant character
+ * Return: pointer to c
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; i < (*(s + i)!= '\0'); i++)
+	{
+		if (*(s + i) == c)
+		      return (s + i);
+	}
+
+	return ('\0');
+}
