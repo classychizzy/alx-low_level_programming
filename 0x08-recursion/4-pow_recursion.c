1843abfb343dd3cf9cf803ee37f2af37e1bd37bb
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-30 15:51:04 -0700

    a function that recursively determines the exponent ofa number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion- a function that determines the exponent of a number
+ * @x: the number whose exponent is to be determined
+ * @y: the exponent of the number
+ * Return: value of the exponent
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		return (_pow_recursion(x, y/2) * _pow_recursion(x, y/2));
+	}
+
+	return (x * _pow_recursion(x, y/2) * _pow_recursion(x, y/2));
+}
