d8cd47efe553ea72f2af85b97deaf3a55320e866
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-03-30 17:36:34 -0700

    function that returns the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * square_root - find the natural square root
+ * @i: number
+ * @j: incrementor
+ * Return: the number found
+ */
+int square_root(int i, int j)
+{
+	if (i * i == j)
+	{
+		return (i);
+	}
+	else if (i * i > j)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (square_root(i + 1, j));
+	}
+}
+/**
+ *  _sqrt_recursion - function that returns the square root of a natural number
+ *  @n: number
+ *  Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	int a = 1;
+
+	return (square_root(n, a));
+}
