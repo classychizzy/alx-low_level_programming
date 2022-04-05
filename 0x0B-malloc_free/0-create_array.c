1d9579113df5d8755a99bead266aababc9cb11ac
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-04-05 14:43:02 -0700

    a program that concatenates strings and returns a newly allocated space for the contents of the concatenated string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str = malloc(size * sizeof(char));
+	char *str = malloc(sizeof(str) * size);
 	unsigned int i;
 
 	if (size == 0 || str == NULL)
