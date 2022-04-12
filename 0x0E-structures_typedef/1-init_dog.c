26c7ff9c5c87bcafb4edfa4ee60fbd90b43b39a7
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-04-12 14:51:29 -0700

    a function that initializes a variable of type dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a  dog
+ * @d: struct dog
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d ->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		
+		printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
+
