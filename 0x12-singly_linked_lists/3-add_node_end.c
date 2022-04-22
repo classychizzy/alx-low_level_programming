59dd0ab36c2fe8bedb2a9566e75551227c1311c5
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-04-22 14:07:33 -0700

    a function that adds a new node at the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,22 +1,50 @@
 #include "lists.h"
+#include <string.h>
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * add_node_end - a function that adds a node to the end of linked list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to string that adds as node
+ * Return: a pointer to new head of list, NULL on failure.
  */
-size_t print_list(const list_t *h)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	size_t nodes;
+	list_t *top, *bottom;
+	int len_;
+	char *string_dup;	
 
-	nodes = 0;
-	while (h != NULL)
+	top =  malloc(sizeof(list_t));
+
+	if (top == NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nodes++;
+		return (NULL);
 	}
-	return (nodes);
+	string_dup = strdup(str);
+	if (string_dup == NULL)
+	{
+		free(top);
+		return (NULL);
+	}
+	
+	len_ = 0;
+	while (str[len_])
+	{
+		len_++;
+	}
+
+	top->len = len_;
+	top->str = string_dup;
+	top->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = top;
+	}
+	else
+	{
+		bottom = *head;
+		while (bottom->next != NULL)
+			bottom = bottom->next;
+		bottom->next = top;
+	}
+	return (*head);
 }
