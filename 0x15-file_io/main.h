ce325e66a0999e0ea0f9a7273a769fccfafcc31d
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-05-02 14:35:32 -0700

    a function thath creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdlib.h>
+#include <sys/type.h>
+#include <sys/stat.h>
+#include <stdlib.h>'
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
