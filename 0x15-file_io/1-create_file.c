b91c4553ea45f3f921e6d424064dd390a6c435fd
root <chizobachukwuoma@gmail.comgit config --global user.name classychizzygit config --global user.email chizobachukwuoma@gmail.com>
2022-05-02 14:52:49 -0700

    a function thath creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
+	rwr = write (fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
